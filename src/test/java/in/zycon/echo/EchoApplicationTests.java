3d8c2b7ba3a7193f6e38c48aafb4ba17419e1dbf
Tibin Lukose <tibin@zycon.in>
2020-02-19 00:00:25 +0400

    Unit Cases

--- src/test/java/in/zycon/echo/EchoApplicationTests.java
+++ src/test/java/in/zycon/echo/EchoApplicationTests.java
@@ -1,13 +1,45 @@
 package in.zycon.echo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(classes = EchoApplication.class)
+@AutoConfigureMockMvc
 class EchoApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    WebApplicationContext webApplicationContext;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void sampleGet() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/test")).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        assertTrue(mvcResult.getResponse().getContentAsString().equalsIgnoreCase("Hello World"));
+    }
+
+    @Test
+    void samplePost() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/test")).andReturn();
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		assertTrue(mvcResult.getResponse().getContentAsString().equalsIgnoreCase("Hello Galaxy"));
+    }
 
 }
